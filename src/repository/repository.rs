175a63dbf26ee4e3f2110ed179dab8d32cfb6051
TobiasBlankJohansson <tobias@annito.se>
2025-02-04 15:52:55 +0100

    refa:refactor task_list to use less code

--- src/repository/repository.rs
+++ src/repository/repository.rs
@@ -3,8 +3,8 @@ use sqlx::PgPool;
 use crate::model::task::Task;
 
 pub async fn task_list(pool: web::Data<PgPool>) -> Vec<Task> {
-    return sqlx::query_as::<_,Task>("SELECT id, name FROM task")
+    sqlx::query_as::<_,Task>("SELECT id, name FROM task")
         .fetch_all(pool.get_ref())
         .await
-        .unwrap();
+        .unwrap()
 }
